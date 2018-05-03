d6f5e63e6e183a8f5b5f71453b919b6e8d4d3f3d
nhokvuive103 <nhokvuive103@gmail.com>
2018-05-03 23:01:33 +0700

    huy commit
    huy

--- GreenShop/app/src/main/java/com/greenshop/greenshop/DataController/DataController.java
+++ GreenShop/app/src/main/java/com/greenshop/greenshop/DataController/DataController.java
@@ -1,4 +1,4 @@
-package com.greenshop.greenshop.DataController;
+﻿package com.greenshop.greenshop.DataController;
 
 import com.greenshop.greenshop.Models.Category;
 import com.greenshop.greenshop.Models.Product;
@@ -9,17 +9,17 @@ public class DataController {
 
     public ArrayList<Product> getAllProducts() {
         ArrayList<Product> products = new ArrayList<>();
-        products.add(new Product("1", "Cây 1", "Cây ...", "Cây mang lại lợi ích", 1, 100000, 110000, new String[] {"cay_canh"}));
-        products.add(new Product("2", "Cây 2", "Cây ...", "Cây mang lại lợi ích", 1, 120000, 110000, new String[] {"cay_canh"}));
-        products.add(new Product("3", "Cây 3", "Cây ...", "Cây mang lại lợi ích", 1, 90000, 110000, new String[] {"cay_canh"}));
-        products.add(new Product("4", "Cây 4", "Cây ...", "Cây mang lại lợi ích", 1, 310000, 110000, new String[] {"cay_canh"}));
-        products.add(new Product("5", "Cây 5", "Cây ...", "Cây mang lại lợi ích", 2, 140000, 110000, new String[] {"cay_canh"}));
-        products.add(new Product("6", "Cây 6", "Cây ...", "Cây mang lại lợi ích", 2, 155000, 110000, new String[] {"cay_canh"}));
-        products.add(new Product("7", "Cây 7", "Cây ...", "Cây mang lại lợi ích", 2, 200000, 110000, new String[] {"cay_canh"}));
-        products.add(new Product("8", "Cây 8", "Cây ...", "Cây mang lại lợi ích", 2, 100000, 110000, new String[] {"cay_canh"}));
-        products.add(new Product("9", "Cây 9", "Cây ...", "Cây mang lại lợi ích", 3, 100000, 110000, new String[] {"cay_canh"}));
-        products.add(new Product("10", "Cây 10", "Cây ...", "Cây mang lại lợi ích", 3, 100000, 110000, new String[] {"cay_canh"}));
-        products.add(new Product("11", "Cây 11", "Cây ...", "Cây mang lại lợi ích", 3, 110000, 210000, new String[] {"cay_canh"}));
+        products.add(new Product("1", "Cây lưỡi hổ", "Cây ...", "Cây mang lại lợi ích", 1, 100000, 110000, new String[] {"cay_canh"}));
+        products.add(new Product("2", "Cây tuyết tùng", "Cây ...", "Cây mang lại lợi ích", 1, 120000, 110000, new String[] {"cay_canh"}));
+        products.add(new Product("3", "Cây sống đời", "Cây ...", "Cây mang lại lợi ích", 1, 90000, 110000, new String[] {"cay_canh"}));
+        products.add(new Product("4", "Cây Lan Ý", "Cây ...", "Cây mang lại lợi ích", 1, 310000, 110000, new String[] {"cay_canh"}));
+        products.add(new Product("5", "Cây dây nhện", "Cây ...", "Cây mang lại lợi ích", 2, 140000, 110000, new String[] {"cay_canh"}));
+        products.add(new Product("6", "Cây nguyệt quế", "Cây ...", "Cây mang lại lợi ích", 2, 155000, 110000, new String[] {"cay_canh"}));
+        products.add(new Product("7", "Cây cọ cảnh", "Cây ...", "Cây mang lại lợi ích", 2, 200000, 110000, new String[] {"cay_canh"}));
+        products.add(new Product("8", "Cây trầu bà", "Cây ...", "Cây mang lại lợi ích", 2, 100000, 110000, new String[] {"cay_canh"}));
+        products.add(new Product("9", "Cây nha đam", "Cây ...", "Cây mang lại lợi ích", 3, 100000, 110000, new String[] {"cay_canh"}));
+        products.add(new Product("10", "Cây dương xỉ", "Cây ...", "Cây mang lại lợi ích", 3, 100000, 110000, new String[] {"cay_canh"}));
+        products.add(new Product("11", "Cây vạn niên thanh", "Cây ...", "Cây mang lại lợi ích", 3, 110000, 210000, new String[] {"cay_canh"}));
         products.add(new Product("12", "Cây 12", "Cây ...", "Cây mang lại lợi ích", 3, 120000, 130000, new String[] {"cay_canh"}));
         products.add(new Product("13", "Cây 13", "Cây ...", "Cây mang lại lợi ích", 3, 130000, 140000, new String[] {"cay_canh"}));
         products.add(new Product("14", "Cây 14", "Cây ...", "Cây mang lại lợi ích", 3, 140000, 160000, new String[] {"cay_canh"}));
